e4ecbcd817448afaa8479fc021c8fd646dc660ac
Halfdan <halfdansc@gmail.com>
2025-04-07 09:04:37 +0200

    fixed pom file and dependencies

--- src/main/java/com/haru_system/p1bot/App.java
+++ src/main/java/com/haru_system/p1bot/App.java
@@ -1,11 +1,20 @@
 package com.haru_system.p1bot;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
+        // makes sure the app is starting
         System.out.println(helloWorld());
+
+        // bot token
+        Dotenv dotenv = Dotenv.load();
+        String botToken = dotenv.get("BOT_TOKEN");
+
+        System.out.println(botToken);
     }
 
     public static String helloWorld() {
