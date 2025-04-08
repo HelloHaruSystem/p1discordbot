7763a1de47d1eac0e07743eefc32ac179a6a6105
Halfdan <halfdansc@gmail.com>
2025-04-08 08:26:09 +0200

    fixed build warnings and updated the pom.xml to be able to use logging

--- src/main/java/com/haru_system/p1bot/App.java
+++ src/main/java/com/haru_system/p1bot/App.java
@@ -16,7 +16,7 @@ public class App
         // bot token and channel id
         Dotenv dotenv = Dotenv.load();
         String botToken = dotenv.get("BOT_TOKEN");
-        String channelId = dotenv.get("P1_GENERAL_CHANEL");
+        String channelId = dotenv.get("DISCORD_TEST_CHANNEL");
 
         // Initialize JDA
         JDA jda = JDABuilder.createDefault(botToken)
